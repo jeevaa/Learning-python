4e1cc7e606f02eae9221986f127164358dddb2e8
jeevaa <adjeevaadurai@gmail.com>
2026-07-08 20:52:45 +0530

    Add files via upload

---
+++ day3.1.py
@@ -0,0 +1,5 @@
+name=input("Enter your name ")
+subject=input("Enter your favourite subject")
+hours=input("Enter the number of hours they study per day")
+
+print(name,"loves",subject,"and studies",hours,"hours a day!")
