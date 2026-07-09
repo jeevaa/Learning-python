eb96d5ba81a95ef2cfe0a04f3fbb7c149bafc59b
jeevaa <adjeevaadurai@gmail.com>
2026-07-09 07:59:35 +0530

    if-elif-else

---
+++ day 4.py
@@ -0,0 +1,8 @@
+number=int(input("Enter a number: "))
+
+if number > 0:
+    print("The number is positive")
+elif number < 5:
+    print("The number is negaative")
+else:
+    print("the number is zero")
